4a53643e8e8137d89aa4202ead6765753b48ec09
bengisu-sahin <bengisusahin99@gmail.com>
2024-01-06 19:23:17 +0300

    user interface is completed for cnn text classification model

---
+++ guide.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Text Emotion Predictor</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar">
+            <h2>MENU</h2>
+            <ul>
+                <li><a href="index.php"><i class="fas fa-home"></i>Home</a></li>
+                <!-- <li><a href="#"><i class="fas fa-user"></i>Profile</a></li> -->
+                <li><a href="about.php"><i class="fas fa-address-card"></i>About</a></li>
+                <!-- <li><a href="#"><i class="fas fa-project-diagram"></i>portfolio</a></li> -->
+                <li><a href="guide.php"><i class="fas fa-blog"></i>Guide</a></li>
+                <li><a href="#"><i class="fas fa-address-book"></i>Contact</a></li>
+                <li><a href="#"><i class="fas fa-map-pin"></i>Map</a></li>
+            </ul>
+            <div class="social_media">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+        <div class="main_content">
+            <div class="header">Guiding Your Mood Chart: Steps to Feeling Good</div>
+            <div class="info" style="align-items: center;">
+                <div>
+                    <div class="info" style="align-items: center; border:2px solid 	#C8C8C8; padding:20px">
+                        <!-- Suggestions for Negative Emotions -->
+                        <div class="suggestions">
+                            <h2>Suggestions for Negative Emotions</h2>
+                            <br>  
+                            <ul>
+                                <li><strong>Spend Time Outside:</strong> Nature walks or visiting a favorite place can have a positive impact on your mental health.</li>  <br>
+                                <li><strong>Take up a Hobby:</strong> Focusing on an activity can reduce stress while keeping you busy. Try hobbies like painting, listening to music or reading books.</li>  <br>
+                                <li><strong>Get Expert Help:</strong> If your negative emotions persist, meeting with an expert can guide you. A therapist or psychologist can help you deal with your emotional problems.</li>  <br>
+                            </ul>
+                        </div>
+                        <br>  <br> 
+                        <!-- Suggestions for Positive Emotions -->
+                        <div class="suggestions">
+                            <h2>Suggestions for Positive Emotions</h2>  <br>
+                            <ul>
+                                <li><strong>Give Thank You:</strong> Focus on the beauty in your life and be filled with gratitude. Writing your good memories in a journal can reinforce this feeling.</li> <br>
+                                <li><strong>Share:</strong> Share your positive emotions with your circle. Collect pleasant memories with your loved ones.</li> <br>
+                                <li><strong>Celebrate Yourself:</strong> Celebrating your achievements can increase your self-confidence. Appreciate even small wins.</li> <br>
+                            </ul>
+                        </div>
+                         <!-- Additional Information or Message -->
+                <div class="suggestions" style=" padding: 20px; border-radius: 10px; text-align: center; margin-top: 20px;">
+                    <p style="font-size: 18px; color: #333;"><em>Stay healthy and take care of your emotional well-being. If you have any concerns, don't hesitate to seek professional help.</em></p>
+                </div>
+                    </div>
+
+                </div>
+               
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
